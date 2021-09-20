20890ca31e4833867f6755c28e67dc1655719bf3
Chris Scott <chrislscott28@gmail.com>
2021-09-20 16:42:00 -0700

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace BYOSA_Utility
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAccountName = new System.Windows.Forms.TextBox();
+            this.txtAccountKey = new System.Windows.Forms.TextBox();
+            this.btnOpenManifest = new System.Windows.Forms.Button();
+            this.lblManifestFile = new System.Windows.Forms.Label();
+            this.txtManifestPath = new System.Windows.Forms.TextBox();
+            this.txtManifestContent = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtContainer = new System.Windows.Forms.TextBox();
+            this.lblContainer = new System.Windows.Forms.Label();
+            this.txtManifestRoot = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(659, 78);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(112, 34);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Account Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Account Key";
+            // 
+            // txtAccountName
+            // 
+            this.txtAccountName.Location = new System.Drawing.Point(186, 40);
+            this.txtAccountName.Name = "txtAccountName";
+            this.txtAccountName.Size = new System.Drawing.Size(150, 31);
+            this.txtAccountName.TabIndex = 3;
+            // 
+            // txtAccountKey
+            // 
+            this.txtAccountKey.Location = new System.Drawing.Point(186, 79);
+            this.txtAccountKey.Name = "txtAccountKey";
+            this.txtAccountKey.Size = new System.Drawing.Size(457, 31);
+            this.txtAccountKey.TabIndex = 4;
+            // 
+            // btnOpenManifest
+            // 
+            this.btnOpenManifest.Location = new System.Drawing.Point(659, 118);
+            this.btnOpenManifest.Name = "btnOpenManifest";
+            this.btnOpenManifest.Size = new System.Drawing.Size(112, 34);
+            this.btnOpenManifest.TabIndex = 5;
+            this.btnOpenManifest.Text = "Open";
+            this.btnOpenManifest.UseVisualStyleBackColor = true;
+            this.btnOpenManifest.Click += new System.EventHandler(this.btnOpenManifest_Click);
+            // 
+            // lblManifestFile
+            // 
+            this.lblManifestFile.AutoSize = true;
+            this.lblManifestFile.Location = new System.Drawing.Point(24, 121);
+            this.lblManifestFile.Name = "lblManifestFile";
+            this.lblManifestFile.Size = new System.Drawing.Size(111, 25);
+            this.lblManifestFile.TabIndex = 6;
+            this.lblManifestFile.Text = "Manifest File";
+            // 
+            // txtManifestPath
+            // 
+            this.txtManifestPath.Location = new System.Drawing.Point(186, 118);
+            this.txtManifestPath.Name = "txtManifestPath";
+            this.txtManifestPath.Size = new System.Drawing.Size(457, 31);
+            this.txtManifestPath.TabIndex = 7;
+            // 
+            // txtManifestContent
+            // 
+            this.txtManifestContent.Location = new System.Drawing.Point(186, 207);
+            this.txtManifestContent.Multiline = true;
+            this.txtManifestContent.Name = "txtManifestContent";
+            this.txtManifestContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtManifestContent.Size = new System.Drawing.Size(585, 279);
+            this.txtManifestContent.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Manifest Content";
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(659, 492);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(112, 34);
+            this.btnValidate.TabIndex = 10;
+            this.btnValidate.Text = "Validate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(186, 541);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResults.Size = new System.Drawing.Size(585, 362);
+            this.txtResults.TabIndex = 11;
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(24, 541);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(150, 25);
+            this.lblResults.TabIndex = 12;
+            this.lblResults.Text = "Validation Results";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(659, 909);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 34);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtContainer
+            // 
+            this.txtContainer.Location = new System.Drawing.Point(493, 40);
+            this.txtContainer.Name = "txtContainer";
+            this.txtContainer.Size = new System.Drawing.Size(150, 31);
+            this.txtContainer.TabIndex = 15;
+            // 
+            // lblContainer
+            // 
+            this.lblContainer.AutoSize = true;
+            this.lblContainer.Location = new System.Drawing.Point(399, 41);
+            this.lblContainer.Name = "lblContainer";
+            this.lblContainer.Size = new System.Drawing.Size(88, 25);
+            this.lblContainer.TabIndex = 14;
+            this.lblContainer.Text = "Container";
+            // 
+            // txtManifestRoot
+            // 
+            this.txtManifestRoot.Location = new System.Drawing.Point(225, 165);
+            this.txtManifestRoot.Name = "txtManifestRoot";
+            this.txtManifestRoot.Size = new System.Drawing.Size(418, 31);
+            this.txtManifestRoot.TabIndex = 17;
+            this.txtManifestRoot.Text = "/AccountDetails/";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(195, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Manifest Root Location";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 953);
+            this.Controls.Add(this.txtManifestRoot);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtContainer);
+            this.Controls.Add(this.lblContainer);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtManifestContent);
+            this.Controls.Add(this.txtManifestPath);
+            this.Controls.Add(this.lblManifestFile);
+            this.Controls.Add(this.btnOpenManifest);
+            this.Controls.Add(this.txtAccountKey);
+            this.Controls.Add(this.txtAccountName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTest);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BYOSA Utility";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAccountName;
+        private System.Windows.Forms.TextBox txtAccountKey;
+        private System.Windows.Forms.Button btnOpenManifest;
+        private System.Windows.Forms.Label lblManifestFile;
+        private System.Windows.Forms.TextBox txtManifestPath;
+        private System.Windows.Forms.TextBox txtManifestContent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtContainer;
+        private System.Windows.Forms.Label lblContainer;
+        private System.Windows.Forms.TextBox txtManifestRoot;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Azure;
+using Azure.Storage.Files.DataLake;
+using Azure.Storage.Files.DataLake.Models;
+using Azure.Storage;
+using System.IO;
+
+
+namespace BYOSA_Utility
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        DataTable manifest = new DataTable();
+        
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+          
+            
+            if (txtAccountName.Text.Length == 0)
+            {
+                MessageBox.Show("The Account Name is missing", "BYOSA Utility");
+                txtAccountName.Focus();
+                return;
+            }
+
+            if (txtAccountKey.Text.Length == 0)
+            {
+                MessageBox.Show("The Account Key is missing", "BYOSA Utility");
+                txtAccountKey.Focus();
+                return;
+            }
+
+            if (txtContainer.Text.Length == 0)
+            {
+                MessageBox.Show("The container name is missing", "BYOSA Utility");
+                txtContainer.Focus();
+                return;
+            }
+
+            try
+            { 
+                StorageSharedKeyCredential sharedKeyCredential = new StorageSharedKeyCredential(txtAccountName.Text, txtAccountKey.Text);
+                DataLakeServiceClient client = GetDataLakeServiceClient(txtAccountName.Text, txtAccountKey.Text, sharedKeyCredential);
+                DataLakeFileSystemClient fileClient = client.GetFileSystemClient(txtContainer.Text);
+                
+                if(client.Uri != null)
+                {
+                    MessageBox.Show("Connection successful: " + client.Uri, "BYOSA Utility");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("There was an error connecting to the container: " + ex.Message, "BYOSA Utility");
+            }
+        }
+        
+        private static DataLakeServiceClient GetDataLakeServiceClient(string accountName, string accountKey, StorageSharedKeyCredential sharedKeyCredential)
+        {
+           
+            string dfsUri = "https://" + accountName + ".dfs.core.windows.net";
+
+            DataLakeServiceClient dataLakeServiceClient = new DataLakeServiceClient(new Uri(dfsUri), sharedKeyCredential);
+
+            return dataLakeServiceClient;
+        }
+
+        private async Task ListContainersInDirectoryAsync(DataLakeFileSystemClient client, StorageSharedKeyCredential sharedKeyCredential, string path)
+        {
+            string directoryName = string.Empty;
+            string fileName = string.Empty;
+            string[] fileNames = new string[0];
+            string[] directories = new string[0];
+            try
+            { 
+                await foreach (PathItem pathItem in client.GetPathsAsync(path, true, false, System.Threading.CancellationToken.None))
+                {
+                    if ((bool)pathItem.IsDirectory)
+                    { 
+                        directories = pathItem.Name.Split('/');
+
+                        for (int k = 0; k < directories.Length; k++)
+                        {
+                     
+                            IAsyncEnumerator<PathItem> enumerator = client.GetPathsAsync(directories[k].ToString()).GetAsyncEnumerator();
+
+                            await enumerator.MoveNextAsync();
+
+                            PathItem item = enumerator.Current;
+
+                            while (item != null)
+                            {
+                                UpdateResults("Directory: " + item.IsDirectory);
+
+                                if (!await enumerator.MoveNextAsync())
+                                {
+                                    break;
+                                }
+
+                                item = enumerator.Current;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        UpdateResults("File found: " + pathItem.Name.Replace(path, ""));
+                    }
+                }
+            }
+            catch (RequestFailedException rfex)
+            {
+                if (rfex.ErrorCode == "PathNotFound")
+                {
+                    UpdateResults("The path was not found for " + path.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                UpdateResults("An error occurred :" + ex.Message);
+            }
+        }
+       
+        private void Main_Load(object sender, EventArgs e)
+        {
+            txtAccountKey.Text = @"yH7t7HbWx1d2gjCsm8rlTfoGR18uTvJ+D6RDgAxDVGbMIGF83YJcn5ll/jMARm83/EfJpJAge4a1251bYdBIbA==";
+            txtAccountName.Text = "clscat";
+            txtContainer.Text = "staging";
+        }
+
+        private void btnOpenManifest_Click(object sender, EventArgs e)
+        {
+            LoadManifest();
+        }
+
+        private void LoadManifest()
+        {
+
+            StringBuilder sb = new StringBuilder();
+            string fileName;
+            string fullFileName;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Multiselect = false;
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Text files (*.txt)|*.txt|JSON files (*.json)|*.json";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+
+                    Cursor.Current = Cursors.WaitCursor;
+
+                    //get the file selected
+                    fileName = openFileDialog.SafeFileName.ToString();
+                    fullFileName = openFileDialog.FileName.ToString();
+                    txtManifestPath.Text = openFileDialog.FileName.ToString();
+                    using (StreamReader r = new StreamReader(fullFileName))
+                    {
+                        
+                        //format the JSON
+                        try
+                        {
+                            var options = new JsonSerializerOptions()
+                            {
+                                WriteIndented = true,
+                                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                            };
+
+                            var jsonElement = JsonSerializer.Deserialize<JsonElement>(r.ReadToEnd().ToString());
+
+                            txtManifestContent.Text = JsonSerializer.Serialize(jsonElement, options);
+                        }
+                        catch (JsonException jx)
+                        {
+                            MessageBox.Show("The manifest file is not in a proper JSON format: " + jx.Message, "BYOSA Utility");
+                        }
+                        catch(Exception ex)
+                        {
+                            MessageBox.Show("An error has occurred: " + ex.Message, "BYOSA Utility");
+                        }
+                        finally
+                        {
+                            Cursor.Current = Cursors.Default;
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool ValidateManifest()
+        {
+            bool valid = false;
+            //read in the JSON from the text box
+            try
+            {
+                if (txtManifestContent.TextLength == 0)
+                {
+                    throw new Exception("The default manifest contents are missing. Load the content and try again");
+                }
+
+                //build out the data table to store the values from the manifest
+                //clear out the structure
+                manifest.Rows.Clear();
+                manifest.Columns.Clear();
+
+                manifest.Columns.Add("EntityName");
+                manifest.Columns.Add("EntityPath");
+                manifest.Columns.Add("RootLocation");
+                manifest.Columns.Add("Regex");
+
+
+                string json = txtManifestContent.Text.ToString();
+                Cursor.Current = Cursors.WaitCursor;
+
+                var options = new JsonDocumentOptions()
+                {
+                };
+
+                JsonElement root;
+                JsonElement entityName;
+                JsonElement entityPath;
+                JsonElement dataPartitions;
+                JsonElement rootLocation;
+                JsonElement regEx;
+
+                JsonDocument document = JsonDocument.Parse(json, options);
+                
+               if (document.RootElement.TryGetProperty("entities", out root) == false)
+               {
+                    throw new Exception("The entitiies node cannot be found");
+               }
+               
+                var entities = root.EnumerateArray();
+                
+                while (entities.MoveNext())
+                {
+                    #region manifestvalidation 
+                    if (entities.Current.TryGetProperty("entityName", out entityName) == false)
+                    {
+                        throw new Exception("The entity name cannot be found");
+                    }
+
+                    if (entities.Current.TryGetProperty("entityPath", out entityPath) == false)
+                    {
+                        throw new Exception("The entity path cannot be found");
+                    }
+
+                    if (entities.Current.TryGetProperty("dataPartitionPatterns", out dataPartitions) == false)
+                    {
+                        throw new Exception("The data partitions pattern node cannot be found");
+                    }
+
+                    #endregion
+                    var dataPartitionPatterns = entities.Current.GetProperty("dataPartitionPatterns");
+                    var partitions = dataPartitionPatterns.EnumerateArray();
+
+                   while (partitions.MoveNext())
+                    {
+
+                        if (partitions.Current.TryGetProperty("rootLocation", out rootLocation) == false)
+                        {
+                            throw new Exception("The root location value cannot be found");
+                        }
+
+                        if (partitions.Current.TryGetProperty("regularExpression", out regEx) == false)
+                        {
+                            throw new Exception("The regular expression value cannot be found");
+                        }
+
+                        manifest.Rows.Add(entityName.ToString(), entityPath.ToString(), rootLocation.ToString(), regEx.ToString());
+                    }
+
+                }
+
+                UpdateResults("Manifest file test successful: " + manifest.Rows.Count.ToString() + " entities found");
+               
+                for (int i=0; i< manifest.Rows.Count; i++)
+                {
+                    UpdateResults(manifest.Rows[i][0].ToString());
+                }
+
+                valid = true;
+
+                return valid;
+            }
+            catch (JsonException jsex)
+            {
+                MessageBox.Show("The Manifest file does not represent a JSON structure: " + jsex.Message, "BYOSA Utility");
+                return valid;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occurred: " + ex.Message, "BYOSA Utility");
+                return valid;
+            }
+            finally
+            {
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        private async void btnValidate_Click(object sender, EventArgs e)
+        {
+
+            string entityPath = string.Empty;
+            string entity = string.Empty;
+
+            if (ValidateManifest())
+            { 
+                DataLakeServiceClient client = GetConnection();
+                DataLakeFileSystemClient fileClient = client.GetFileSystemClient(txtContainer.Text);
+                StorageSharedKeyCredential sharedKeyCredential = new StorageSharedKeyCredential(txtAccountName.Text, txtAccountKey.Text);
+
+                for (int i=0; i< manifest.Rows.Count; i++)
+                {
+                    if (manifest.Rows[i][2].ToString().Length > 0)
+                    { 
+                        entity = manifest.Rows[i][0].ToString();
+                        entityPath = txtManifestRoot.Text + manifest.Rows[i][2].ToString();
+
+                        UpdateResults("Testing " + entity + " at " + entityPath);
+                        await ListContainersInDirectoryAsync(fileClient, sharedKeyCredential, entityPath);
+                    }
+                }
+            }
+        }
+
+        private DataLakeServiceClient GetConnection()
+        {
+
+            if (txtAccountName.Text.Length == 0)
+            {
+                throw new Exception("The Account Name is missing");
+            }
+
+            if (txtAccountKey.Text.Length == 0)
+            {
+
+                throw new Exception("The Account Key is missing"); 
+             }
+
+            if (txtContainer.Text.Length == 0)
+            {
+                throw new Exception("The container name is missing");
+            }
+
+            try
+            {
+                StorageSharedKeyCredential sharedKeyCredential = new StorageSharedKeyCredential(txtAccountName.Text, txtAccountKey.Text);
+                DataLakeServiceClient client = GetDataLakeServiceClient(txtAccountName.Text, txtAccountKey.Text, sharedKeyCredential);
+               
+                if (client.Uri != null)
+                {
+                    UpdateResults("Connection successful: " + client.Uri);
+                }
+                return client;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an error connecting to the container: " + ex.Message, "BYOSA Utility");
+                return null;
+                
+            }
+        }
+
+        private void UpdateResults(string message)
+        {
+            if(txtResults.TextLength > 0)
+                txtResults.AppendText(Environment.NewLine);
+    
+            txtResults.AppendText(message);
+        }
+    }
+}
+    
